8f1d2b3512b4c97f04803011b85012fc7a29ab81
Samuel Li <samuelli4521@gmail.com>
2022-07-17 23:15:51 +0800

    clear files

--- js/components/Avatar.js
+++
@@ -1,7 +0,0 @@
-const Avatar = props => {
-  return (
-    <img className="avatar" src="http://res.cloudinary.com/mashedpotats/image/upload/v1495157325/github-page-profile_wclxp7.jpg"></img>
-  )
-}
-  
-export default Avatar;
